95f057ea1b119936bbf6b55ca2fd5f8e0c4bfca8
Reebloz <smacka21@student.aau.dk>
2022-12-05 14:53:10 +0100

    Welp, let the merge commence...

--- P3-Project/Models/Sql/MySql.cs
+++ P3-Project/Models/Sql/MySql.cs
@@ -524,46 +524,9 @@ public class MySqlDB : DataBase {
 		}
 		return rtn;
 	}
-	public List<T> ReadLastFromTable<T>(string name, string where, Func<IList<object>, T> initializer) where T : notnull{
-		cmd.CommandText = $"SELECT LAST * FROM {name} WHERE {where}";
-		Console.WriteLine(cmd.CommandText);
-		conn.Open();
-
-		var rtn = new List<T>();
-		
-		var sdr = cmd.ExecuteReader();
-		ReadToArrayFunc(sdr, rtn, initializer);
-		conn.Close();
-		return rtn;
-	}
-	public List<T> ReadLastFromTable<T>(string name, IEnumerable<string> columns, Func<IList<object>, T> initializer ) where T : notnull{
-		cmd.CommandText = $"SELECT LAST {string.Join(',', columns )} FROM {name}";
-		Console.WriteLine(cmd.CommandText);
-		conn.Open();
-
-		var rtn = new List<T>();
-		
-		var sdr = cmd.ExecuteReader();
-		ReadToArrayFunc(sdr, rtn, initializer);
-		conn.Close();
-		return rtn;
-
-	}
-	public List<T> ReadLastFromTable<T>(string name, IEnumerable<string> columns, string where, Func<IList<object>, T> initializer ) where T : notnull{
-		cmd.CommandText = $"SELECT LAST {string.Join(',', columns )} FROM {name} WHERE {where}";
-		Console.WriteLine(cmd.CommandText);
-		conn.Open();
-
-		var rtn = new List<T>();
-		try {
-			var sdr = cmd.ExecuteReader();
-			ReadToArrayFunc(sdr, rtn, initializer);
-		} finally {	
-			conn.Close();
-		}
-		return rtn;
-	}
-
+	
+	
+	
     public int GetStockAmount(string tableName)
     {
         cmd.CommandText = "SELECT SUM(Stock) FROM " + tableName;
